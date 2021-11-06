ce31f66accc7c56e6f1f7bdf137d49fa09c2eb76
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-11-06 06:16:43 +0000

    New namespace syntax

--- WebApp1/Controllers/HomeController.cs
+++ WebApp1/Controllers/HomeController.cs
@@ -2,49 +2,48 @@
 using Microsoft.Extensions.Options;
 using System.Diagnostics;
 
-namespace WebApp1.Controllers
+namespace WebApp1.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IOptionsMonitor<Api1Settings> _api1SettingsOptionsAccessor;
+
+    public HomeController(IHttpClientFactory httpClientFactory,
+        IOptionsMonitor<Api1Settings> api1SettingsOptionsAccessor)
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IOptionsMonitor<Api1Settings> _api1SettingsOptionsAccessor;
+        ArgumentNullException.ThrowIfNull(httpClientFactory);
+        ArgumentNullException.ThrowIfNull(api1SettingsOptionsAccessor);
 
-        public HomeController(IHttpClientFactory httpClientFactory,
-            IOptionsMonitor<Api1Settings> api1SettingsOptionsAccessor)
+        _httpClientFactory = httpClientFactory;
+        _api1SettingsOptionsAccessor = api1SettingsOptionsAccessor;
+    }
+
+    public async Task<IActionResult> Index(string name)
+    {
+        using HttpClient httpClient = _httpClientFactory.CreateClient();
+        string getUrl = $"{_api1SettingsOptionsAccessor.CurrentValue.Api1Url}?name={name}";
+        HttpResponseMessage response = await httpClient.GetAsync(getUrl);
+        if (response.IsSuccessStatusCode)
         {
-            ArgumentNullException.ThrowIfNull(httpClientFactory);
-            ArgumentNullException.ThrowIfNull(api1SettingsOptionsAccessor);
-
-            _httpClientFactory = httpClientFactory;
-            _api1SettingsOptionsAccessor = api1SettingsOptionsAccessor;
+            string message = await response.Content.ReadAsStringAsync();
+            ViewData["Message"] = message;
         }
-
-        public async Task<IActionResult> Index(string name)
+        else
         {
-            using HttpClient httpClient = _httpClientFactory.CreateClient();
-            string getUrl = $"{_api1SettingsOptionsAccessor.CurrentValue.Api1Url}?name={name}";
-            HttpResponseMessage response = await httpClient.GetAsync(getUrl);
-            if (response.IsSuccessStatusCode)
-            {
-                string message = await response.Content.ReadAsStringAsync();
-                ViewData["Message"] = message;
-            }
-            else
-            {
-                ViewData["Message"] = "No message found";
-            }
-            return View();
+            ViewData["Message"] = "No message found";
         }
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
