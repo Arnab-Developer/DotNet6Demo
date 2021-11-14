52afcba15579274df1ea652af010a3dbc293ef9f
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-11-14 12:07:31 +0530

    wip

--- Api1Test/GreetEndpointTest.cs
+++ Api1Test/GreetEndpointTest.cs
@@ -1,6 +1,8 @@
+// Got the unit test idea from https://github.com/dotnet/aspnetcore/issues/37502
+
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Result;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,24 +24,31 @@ public class GreetEndpointTest
     [Fact]
     public async Task Can_GetMessage_ReturnProperMessage()
     {
-        // Got the unit test idea from https://github.com/dotnet/aspnetcore/issues/37502
+        Mock<IGreetService> greetServiceMock = new();
+        greetServiceMock.Setup(s => s.GetGreetMessage(It.IsAny<string>())).Returns("Hello Test User");
 
-        IGreetService greetService = new GreetService();
-        IResult result = GreetEndpointExtensions.GetMessage("Test User", greetService);
+        IResult result = GreetEndpointExtensions.GetMessage("Test User", greetServiceMock.Object);
         HttpContext mockHttpContext = CreateMockHttpContext();
         await result.ExecuteAsync(mockHttpContext);
         mockHttpContext.Response.Body.Position = 0;
-        using (StreamReader reader = new StreamReader(mockHttpContext.Response.Body, Encoding.UTF8))
-        {
-            Assert.Equal("\"Hello Test User\"", reader.ReadToEnd());
-        }
+        using StreamReader reader = new(mockHttpContext.Response.Body, Encoding.UTF8);
 
+        Assert.Equal("\"Hello Test User\"", reader.ReadToEnd());
         Assert.Equal(200, mockHttpContext.Response.StatusCode);
     }
 
     [Fact]
-    public void Can_GetMessage_ThrowException()
+    public async Task Can_GetMessage_ThrowException()
     {
+        Mock<IGreetService> greetServiceMock = new();
+        greetServiceMock.Setup(s => s.GetGreetMessage(string.Empty)).Throws<ArgumentNullException>();
 
+        IResult result = GreetEndpointExtensions.GetMessage(string.Empty, greetServiceMock.Object);
+        HttpContext mockHttpContext = CreateMockHttpContext();
+        await result.ExecuteAsync(mockHttpContext);
+        mockHttpContext.Response.Body.Position = 0;
+        using StreamReader reader = new(mockHttpContext.Response.Body, Encoding.UTF8);
+
+        Assert.Equal(500, mockHttpContext.Response.StatusCode);
     }
 }
